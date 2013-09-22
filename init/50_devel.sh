314d47e6c21fe6c24712004fcd6d7749659cc00f
Mikołaj Pastuszko <mikolaj.pastuszko@gmail.com>
2013-09-22 21:53:58 +0200

    rbenv disabled

--- init/50_devel.sh
+++ init/50_devel.sh
@@ -32,27 +32,27 @@ if [[ "$(type -P npm)" ]]; then
 fi
 
 # Install Ruby.
-if [[ "$(type -P rbenv)" ]]; then
-  versions=(2.0.0-p247)
+# if [[ "$(type -P rbenv)" ]]; then
+#   versions=(2.0.0-p247)
 
-  list="$(to_install "${versions[*]}" "$(rbenv whence ruby)")"
-  if [[ "$list" ]]; then
-    e_header "Installing Ruby versions: $list"
-    for version in $list; do rbenv install "$version"; done
-    [[ "$(echo "$list" | grep -w "${versions[0]}")" ]] && rbenv global "${versions[0]}"
-    rbenv rehash
-  fi
-fi
+#   list="$(to_install "${versions[*]}" "$(rbenv whence ruby)")"
+#   if [[ "$list" ]]; then
+#     e_header "Installing Ruby versions: $list"
+#     for version in $list; do rbenv install "$version"; done
+#     [[ "$(echo "$list" | grep -w "${versions[0]}")" ]] && rbenv global "${versions[0]}"
+#     rbenv rehash
+#   fi
+# fi
 
 # Install Gems.
-if [[ "$(type -P gem)" ]]; then
-  gems=(bundler awesome_print pry lolcat)
+# if [[ "$(type -P gem)" ]]; then
+#   gems=(bundler awesome_print pry)
 
-  list="$(to_install "${gems[*]}" "$(gem list | awk '{print $1}')")"
-  if [[ "$list" ]]; then
-    e_header "Installing Ruby gems: $list"
-    gem install $list
-    rbenv rehash
-  fi
-fi
+#   list="$(to_install "${gems[*]}" "$(gem list | awk '{print $1}')")"
+#   if [[ "$list" ]]; then
+#     e_header "Installing Ruby gems: $list"
+#     gem install $list
+#     rbenv rehash
+#   fi
+# fi
 
